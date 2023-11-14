e49ed076986c0a274ee6f0bf25f4cff757760e4e
gustavwiese <gusse12345@gmail.com>
2023-11-14 13:30:39 +0100

    homepage done and some basic css

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 export default function Homepage() {
   return (
-    <div>
-      <h1>Dette er vores startside</h1>
-      <h2>Lavet af Gustav, Benjamin, Sham og Emil</h2>
+    <div className="Homepage">
+      <h1>Dette er vores startside 😍</h1>
+      <h2>Lavet af Gustav, Benjamin, Sham og Emil 🙌🥳</h2>
     </div>
   );
 }
