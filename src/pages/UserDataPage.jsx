e096a66dfe977e91a2a998eb98db5d4cb5354e56
emsc0001 <124031667+emsc0001@users.noreply.github.com>
2023-11-14 14:15:44 +0100

    2

--- src/pages/UserDataPage.jsx
+++ src/pages/UserDataPage.jsx
@@ -1,37 +1,32 @@
-import React from "react"
+import React, { useState, useEffect } from "react";
 
-export default function User ({ id, name, username, email, age }){
-    return (
-        <div style={{ display: 'flex', marginBottom: '10px' }}>
-            <div style={{ marginRight: '10px' }}>{id}</div>
-            <div style={{ marginRight: '10px' }}>{name}</div>
-            <div style={{ marginRight: '10px' }}>{username}</div>
-            <div style={{ marginRight: '10px' }}>{email}</div>
-            <div>{age}</div>
-        </div>
-    );
+export default function Table({ data }) {
+  console.log("Received data prop in Table component:", data);
 
-function UserDataPage( id, user) {
-    
-    const userData = [
-        { id: 1, username: 'User1', email: 'user1@example.com', age: 28 },
-        { id: 2, username: 'User2', email: 'user2@example.com', age: 35 },
-        { id: 3, username: 'User3', email: 'user3@example.com', age: 42 },
-    ];
-
-    return (
-        <>
-            <h1>Dette er brugerfladen</h1>
-            {userData.map(user => (
-                <User 
-                    key={user.id} 
-                    id={user.id} 
-                    username={user.username} 
-                    email={user.email} 
-                    age={user.age} 
-                />
-            ))}
-        </>
-    );
-}
+  return (
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>NAME</th>
+            <th>USERNAME</th>
+            <th>MAIL</th>
+            <th>AGE</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((user) => (
+            <tr key={user.id}>
+              <td>{user.id}</td>
+              <td>{user.name}</td>
+              <td>{user.username}</td>
+              <td>{user.mail}</td>
+              <td>{user.age}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 }
