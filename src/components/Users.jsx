1fed0a67f97ce66769aa278e36e9e78f857506dc
emsc0001 <124031667+emsc0001@users.noreply.github.com>
2023-11-14 14:22:34 +0100

    Update Users.jsx

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,1 +1,23 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Table from "./Table";
+
+export default function Users() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await fetch("http://localhost:3000/users");
+        const data = await res.json();
+        setData(data);
+        // console.log(data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return <Table data={data} />;
+}
