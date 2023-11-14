4b0fa835a9b3068e6889fad95ff153abd5a8857c
Benjamin-Harris1 <beha1503@gmail.com>
2023-11-14 14:47:18 +0100

    fix last path

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import Homepage from "../pages/Homepage";
 import UserDataPage from "../pages/UserDataPage";
+import UserCreate from "../pages/UserCreate";
 import Navbar from "./Navbar";
 
 export default function App() {
@@ -10,6 +11,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/users" element={<UserDataPage />} />
+        <Route path="/newuser" element={<UserCreate />} />
       </Routes>
     </div>
   );
