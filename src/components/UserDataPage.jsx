b1430428ca414a57c7f9e4095178bb791e5c868c
emsc0001 <124031667+emsc0001@users.noreply.github.com>
2023-11-14 13:51:04 +0100

    Create userdatapage

---
+++ src/components/UserDataPage.jsx
@@ -0,0 +1,37 @@
+import React from "react"
+import User from 
+
+export default function User ({ id, username, email, age}){
+    return (
+        <div style={{ display: 'flex', marginBottom: '10px' }}>
+            <div style={{ marginRight: '10px' }}>{id}</div>
+            <div style={{ marginRight: '10px' }}>{username}</div>
+            <div style={{ marginRight: '10px' }}>{email}</div>
+            <div>{age}</div>
+        </div>
+    );
+
+export default function UserDataPage() {
+    
+    const userData = [
+        { id: 1, username: 'User1', email: 'user1@example.com', age: 28 },
+        { id: 2, username: 'User2', email: 'user2@example.com', age: 35 },
+        { id: 3, username: 'User3', email: 'user3@example.com', age: 42 },
+    ];
+
+    return (
+        <>
+            <h1>Dette er brugerfladen</h1>
+            {userData.map(user => (
+                <User 
+                    key={user.id} 
+                    id={user.id} 
+                    username={user.username} 
+                    email={user.email} 
+                    age={user.age} 
+                />
+            ))}
+        </>
+    );
+}
+}
