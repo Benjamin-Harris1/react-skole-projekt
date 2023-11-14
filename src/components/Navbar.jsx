73b39143d2a9024578aa13139683de4c5e7ce213
gustavwiese <gusse12345@gmail.com>
2023-11-14 13:40:40 +0100

    navbar done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,11 @@
-import React from 'react'
+import React from "react";
 
 export default function Navbar() {
   return (
-    <div>Navbar</div>
-  )
+    <div className="Navbar">
+      <p>Hjem</p>
+      <p>Brugere</p>
+      <p>Tilføj brugere</p>
+    </div>
+  );
 }
